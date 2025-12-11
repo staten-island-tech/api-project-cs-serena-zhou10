0f46811613ddb0140d538361b0028458da964940
Serena <serenazhou4268@gmail.com>
2025-12-11 12:30:25 -0500

    project

---
+++ main.js
@@ -0,0 +1,18 @@
+async function getData(/*name*/) {
+  try {
+    // go get data
+    const response = await fetch(/*paste the link of api using`` /${name}*/);
+    // handle errors
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      // makes the response into json data we can use
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(/*"name"*/);
